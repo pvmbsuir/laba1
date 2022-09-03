5b68551a171390ce5cd7cfc40dcad5d355c50396
Polina <p.pupsic2.0.2@gmail.com>
2022-09-03 19:36:20 +0300

    tests for new operations

---
+++ src/test/Tests.java
@@ -0,0 +1,27 @@
+package test;
+
+import demo.parallel.Complex;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Tests {
+    @Test
+    public void testComplexSub() throws Exception{
+        Complex a = new Complex(9, 3);
+        Complex b = new Complex(2,2);
+        a.sub(b);
+        assertEquals(7, a.getRe());
+        assertEquals(1, a.getIm());
+    }
+
+    @Test
+    public void testComplexDivide() throws Exception{
+        Complex a = new Complex(9, 3);
+        Complex b = new Complex(2,2);
+        a.divide(b);
+        assertEquals(3, a.getRe());
+        assertEquals(-1.5, a.getIm());
+    }
+}
