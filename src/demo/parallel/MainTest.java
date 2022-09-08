b809ba51aecf68a8a21614f0f893835423ed0095
Vladislav <vlad_osipov2002@mail.ru>
2022-09-08 09:13:44 +0300

    Unit test (Complex.java)

---
+++ src/demo/parallel/MainTest.java
@@ -0,0 +1,18 @@
+package demo.parallel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void start() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void stop() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
