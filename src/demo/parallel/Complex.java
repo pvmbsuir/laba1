f6aa7bf2961db27cf50cf215b2b6f366adbb5a83
Anatoliy030501 <20davydyonok02@gmail.com>
2022-09-13 10:20:07 +0300

    complex

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -60,6 +60,22 @@ public class Complex {
         im = imag;
     }
 
+    public Complex sub(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    public Complex div(Complex b)
+    {
+        Complex a = this;
+        double real = (a.re * b.re + a.im * b.im)/(b.re*b.re+b.im*b.im);
+        double imag = (a.im * b.re - a.re * b.im)/(b.re*b.re+b.im*b.im);
+        re = real;
+        im = imag;
+        return this;
+    }
+
     /**
      * Add operation.
      * @param b summand
