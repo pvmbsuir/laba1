ed1e2673554569ae88d190e8a7a32701950b03bc
Mikhail Kutovenko <70900496+mkhlktvnk@users.noreply.github.com>
2022-09-13 22:05:49 +0300

    divide operation added

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -42,18 +42,19 @@ package demo.parallel;
  * required for a production-quality application, such as security checks,
  * input validation and proper error handling, might not be present in
  * this sample code.</i>
+ *
  * @author Alexander Kouznetsov, Tristan Yan
  */
 public class Complex {
-    
+
     private double re;   // the real part
     private double im;   // the imaginary part
 
-    /** 
+    /**
      * create a new object with the given real and imaginary parts
-     * 
+     *
      * @param real a complex number real part
-     * @param imag a complex number imaginary part 
+     * @param imag a complex number imaginary part
      */
     public Complex(double real, double imag) {
         re = real;
@@ -62,6 +63,7 @@ public class Complex {
 
     /**
      * Add operation.
+     *
      * @param b summand
      * @return this Complex object whose value is (this + b)
      */
@@ -77,9 +79,18 @@ public class Complex {
         return this;
     }
 
+    public Complex divide(Complex b) {
+        if ((b.re * b.re + b.im * b.im) != 0) {
+            re = (re * b.re + im * b.im) / (b.re * b.re + b.im * b.im);
+            im = (im * b.re + re * b.im) / (b.re * b.re + b.im * b.im);
+        }
+        return this;
+    }
+
     /**
      * Multiply operation.
-     * @param  b multiplier
+     *
+     * @param b multiplier
      * @return this Complex object whose value is this * b
      */
     public Complex times(Complex b) {
@@ -92,10 +103,11 @@ public class Complex {
     }
 
     /**
-     * Square of Complex object's length, we're using square of length to 
+     * Square of Complex object's length, we're using square of length to
      * eliminate the computation of square root
+     *
      * @return square of length
-    */
+     */
     public double lengthSQ() {
         return re * re + im * im;
     }
