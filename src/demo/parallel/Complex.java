d6c53076db03f0b8073b64a6b3c48d50d2d3ca1a
Yegor Andrievsky <egora0788@gmail.com>
2022-09-13 19:13:55 +0300

    update Complex: add minus, getRe and getIm functions

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,20 @@ public class Complex {
         return this;
     }
 
+    public Complex minus(Complex b){
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm(){
+        return im;
+    }
+
     /**
      * Multiply operation.
      * @param  b multiplier
@@ -93,4 +107,6 @@ public class Complex {
     public double lengthSQ() {
         return re * re + im * im;
     }
+
+
 }
