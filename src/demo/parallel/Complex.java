fe439d1b687fdadca807c1cff2c45816c504681d
Mikhail Kutovenko <70900496+mkhlktvnk@users.noreply.github.com>
2022-09-13 22:22:37 +0300

    multiply operation added

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -87,6 +87,12 @@ public class Complex {
         return this;
     }
 
+    public Complex multiply(Complex b) {
+        re *= b.re;
+        im *= b.im;
+        return this;
+    }
+
     /**
      * Multiply operation.
      *
