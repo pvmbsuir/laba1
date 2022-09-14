cedee51892c4f6c5e7b483e3f85c095538edac85
Sergey Luzhinsky <sergey.luzhinsky@gmail.com>
2022-09-14 15:31:54 +0300

    Add Unit Tests

---
+++ src/Tests/UnitTests.java
@@ -0,0 +1,27 @@
+package Tests;
+
+import demo.parallel.Complex;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class UnitTests {
+
+    @Test
+    public void testPlus() throws Exception {
+        Complex a = new Complex(6, 6);
+        Complex b = new Complex(4,8);
+        a.plus(b);
+        assertEquals(10, a.getRe());
+        assertEquals(14, a.getIm());
+    }
+
+    @Test
+    public void testMinus() throws Exception{
+        Complex a = new Complex(3, 8);
+        Complex b = new Complex(1,15);
+        a.minus(b);
+        assertEquals(2, a.getRe());
+        assertEquals(-7, a.getIm());
+    }
+
+
+}
