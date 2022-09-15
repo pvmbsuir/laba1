27197c653d184a668a0d1f71c522b35af0268b91
Ilya Shall <shallilya20@gmail.com>
2022-09-15 19:57:22 +0300

    ComplexChange

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,9 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex b = new Complex (1, 0);
         do {
-            c = c.times(c).plus(comp).times(c);
+            c = c.times(c).plus(comp).times(c.minus(b)).minus(comp).div(b);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
