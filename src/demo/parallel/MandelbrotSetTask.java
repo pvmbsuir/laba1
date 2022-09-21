a699b1bb97464bea0460a6db3f735adab05b884f
Andrey Sikorin <amaomi.prog@gmail.com>
2022-09-21 20:31:04 +0300

    Add Complex subtraction and division

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,11 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).times(c).plus(comp);
+            c = c.times(c)
+                 .times(c)
+                 .divide(new Complex(1, -1))
+                 .plus(comp)
+                 .divide(comp);
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
