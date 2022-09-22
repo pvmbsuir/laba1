9ed64b83cf0157528d0f4c204c4e1d297a800cd8
Egor Shinkarevich <yahorshinkarevich7@gmail.com>
2022-09-22 19:48:02 +0300

    added divide operation&test

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -30,52 +30,30 @@
  */
 package demo.parallel;
 
-
-/**
- * A complex number is a number that can be expressed in the form a + b * i, where
- * a and b are real numbers and i is the imaginary unit, which satisfies the
- * equation i ^ 2 = -1. a is the real part and b is the imaginary part of the
- * complex number.
- * <p><i>
- * This source code is provided to illustrate the usage of a given feature
- * or technique and has been deliberately simplified. Additional steps
- * required for a production-quality application, such as security checks,
- * input validation and proper error handling, might not be present in
- * this sample code.</i>
- * @author Alexander Kouznetsov, Tristan Yan
- */
 public class Complex {
     
     private double re;   // the real part
     private double im;   // the imaginary part
 
-    /** 
-     * create a new object with the given real and imaginary parts
-     * 
-     * @param real a complex number real part
-     * @param imag a complex number imaginary part 
-     */
     public Complex(double real, double imag) {
         re = real;
         im = imag;
     }
 
-    /**
-     * Add operation.
-     * @param b summand
-     * @return this Complex object whose value is (this + b)
-     */
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
     public Complex plus(Complex b) {
         re += b.re;
         im += b.im;
         return this;
     }
 
-    /**
-     * Multiply operation.
-     * @param  b multiplier
-     * @return this Complex object whose value is this * b
-     */
     public Complex times(Complex b) {
         Complex a = this;
         double real = a.re * b.re - a.im * b.im;
@@ -85,64 +63,33 @@ public class Complex {
         return this;
     }
 
-    /**
-     * Square of Complex object's length, we're using square of length to 
-     * eliminate the computation of square root
-     * @return square of length
-    */
     public double lengthSQ() {
         return re * re + im * im;
     }
-    
+
         public Complex minus(Complex b) {
         re -= b.re;
         im -= b.im;
         return this;
     }
 
-    /**
-     * Divide operation.
-     * @param  b divider
-     * @return this Complex object whose value is this/b
-     */
     public Complex divide(Complex b){
-        Complex conjugateNumber = getConjugateNumber(b);
-        Complex numeratorOfTheFraction = this.times(conjugateNumber);
-        Complex denominatorOfTheFraction = b.times(conjugateNumber);
-
-        try {
-            return new Complex(numeratorOfTheFraction.re / denominatorOfTheFraction.re,
-                    numeratorOfTheFraction.im / denominatorOfTheFraction.re);
-        }
-
-        catch (ArithmeticException a){
-            throw new ArithmeticException("Error!Division by 0.");
-        }
+        Complex a = this;
+        double real = a.re / b.re - a.im / b.im;
+        double imag = a.re / b.im + a.im / b.re;
+        re = real;
+        im = imag;
+        return this;
     }
 
-    /**
-     * Get conjugate number
-     * @param number
-     * @return number with other im
-     */
     public Complex getConjugateNumber(Complex number){
         return new Complex(number.re,-number.im);
     }
 
-        /**
-     * Cos(z) operation.
-     * @return cos(z)
-     */
     public Complex getCos(){
         return new Complex(Math.cos(this.re)*Math.cosh(this.im), -Math.sin(this.re)*Math.sinh(this.im));
     }
 
-    /**
-     * My Math.pow for complex number
-     * @param degree for pow complex number
-     * @throws ArithmeticException if will be division by 0
-     * @return complex number in this degree
-     */
     public Complex pow(int degree){
 
         double module = Math.sqrt(lengthSQ());
--- src/demo/parallel/Complex.test.java
+++ src/demo/parallel/Complex.test.java
@@ -4,7 +4,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ComplexTest {
 
@@ -27,5 +29,15 @@ class ComplexTest {
                 "Test for calculating cos(z) with arg = PI/6");
         assertTrue(new Complex(2*Math.PI/3 ,0).getCos().equals(new Complex(-0.5,0)),
                 "Test for calculating cos(z) with arg =2PI/3");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void checkDivide() throws Exception {
+        Complex a = new Complex(3, 3);
+        Complex b = new Complex(2,4);
+        a.divide(b);
+
+        assertEquals(0.75, a.getRe());
+        assertEquals(2.25, a.getIm());
     }
 }
