d6016bb6486d5b64e5dee7713f314939be3b6f39
Ilya Charauko <75255156+MirrSs@users.noreply.github.com>
2022-09-23 17:24:46 +0300

    minor update

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,11 +275,7 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            //c = c.times(c).plus(comp);
-            c = c.plus(c)
-                    .times(c.plus(comp))
-                    .times(c)
-                    .dividedBy(new Complex(0, 7));
+            c = c.plus(c).times(c.plus(comp)).times(c).divide(new Complex(0, 7));
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
--- src/demo/parallel/Tests.java
+++ src/demo/parallel/Tests.java
@@ -6,26 +6,27 @@ import org.junit.jupiter.api.Test;
 public class Tests {
 
     @Test
-    void complexPlusOperationTest() {
-        Complex complex = new Complex(12, 2);
-        complex = complex.plus(new Complex(10, 1));
+    void PlusTest() {
+        Complex complex = new Complex(12, 4);
+        complex = complex.plus(new Complex(7, 5));
 
-        assert complex.getReal() == 22 && complex.getImaginary() == 3;
+        assert complex.getReal() == 19 && complex.getImaginary() == 9;
     }
 
     @Test
-    void complexDivideOperationTest() {
+    void DivideTest() {
         Complex complex = new Complex(12, 2);
-        complex = complex.dividedBy(new Complex(2, 2));
+        complex = complex.divide(new Complex(2, 2));
 
         assert complex.getReal() == 3.5f && complex.getImaginary() == -2.5f;
     }
 
     @Test
-    void squareComplexTest() {
+    void DivideByZeroTest(){
         Complex complex = new Complex(12, 2);
+        complex = complex.divide(new Complex(0, 2));
 
-        assert complex.lengthSQ() == 12*12 + 2*2;
+        assert !Double.isNaN(complex.getReal());
+        assert !Double.isNaN(complex.getImaginary());
     }
-
 }
