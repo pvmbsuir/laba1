4596562ced4469b88bad57b6a4eba2a87ca0f363
Drrrambix <105435215+Drrrambix@users.noreply.github.com>
2022-09-26 22:20:18 +0300

    added Unit tests to check if new operations are executed correctly

---
+++ src/demo/parallel/ComplexTest.java
@@ -0,0 +1,28 @@
+package demo.parallel;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ComplexTest {
+
+    @Test
+    public void testDiv() {
+
+        Complex a = new Complex(4, -2);
+        Complex b = new Complex(4, -2);
+        Complex expected = new Complex(1, 0);
+        Complex divResult = a.div(b);
+        Assert.assertEquals(expected, divResult);
+    }
+
+    @Test
+    public void testMinus() {
+
+        Complex a = new Complex(3, -1);
+        Complex b = new Complex(1, -2);
+        Complex expected = new Complex(2, 1);
+        Complex minusResult = a.minus(b);
+        Assert.assertEquals(expected, minusResult);
+    }
+
+}
