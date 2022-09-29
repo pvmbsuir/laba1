143cbcedb0f22a9c7b8dbef7eb026d18e332da9e
Drrrambix <vdambovskiy@gmail.com>
2022-09-29 20:57:31 +0300

    executed correctly

--- src/demo/parallel/ComplexTest.java
+++ src/demo/parallel/ComplexTest.java
@@ -9,10 +9,20 @@ public class ComplexTest {
     public void testDiv() {
 
         Complex a = new Complex(4, -2);
-        Complex b = new Complex(4, -2);
+        Complex b = new Complex(0, 0);
+
         Complex expected = new Complex(1, 0);
+
         Complex divResult = a.div(b);
-        Assert.assertEquals(expected, divResult);
+
+        if ( b.equals(new Complex(0,0)) )
+        {
+            String str = String.format("Неявный результат");
+            System.out.println(str);
+
+        }
+        else {
+        Assert.assertEquals(expected, divResult);}
     }
 
     @Test
