2ba2772277d13e8a656177130ef5e82c11ec9793
mattik <makshds123@gmail.com>
2022-10-05 16:51:11 +0300

    добавление юнит теста

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -100,4 +100,10 @@ public class Complex {
     public double lengthSQ() {
         return re * re + im * im;
     }
+    public double getReal(){
+        return re;
+    }
+    public double getImaginary(){
+        return im;
+    }
 }
